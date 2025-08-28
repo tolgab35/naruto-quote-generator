3336d45e9a1b92ca150067649d87b05b0c351fa6
Tolga <tolgaboz350@gmail.com>
2025-08-28 15:19:17 +0300

    feat: fetch quotes.json on button click and render random quote to DOM

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 async function loadQuotes() {
   const url = "quotes.json";
-
   const response = await fetch(url);
   const data = await response.json();
-
-  console.log(data);
+  return data;
 }
 
-loadQuotes();
+document.getElementById("new-quote").addEventListener("click", async () => {
+  const quotes = await loadQuotes();
+
+  const random = quotes[Math.floor(Math.random() * quotes.length)];
+
+  document.getElementById("quote").textContent = random.quote;
+  document.getElementById("author").textContent = random.author;
+});
